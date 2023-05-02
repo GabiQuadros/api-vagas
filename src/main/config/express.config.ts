80f269b9f6604ab92ef1de28bf56fff67b44159e
Gabriela Quadros <ngabiquadros@gmail.com>
2023-05-02 20:37:08 -0300

    under construction

--- src/main/config/express.config.ts
+++ src/main/config/express.config.ts
@@ -1,10 +1,24 @@
 import cors from "cors";
+import cors from "cors";
 import express from "express";
+import { recrutadorRoutes } from "../../app/features/recrutador/routes/recrutador.routes";
+import { loginRoutes } from "../../app/features/login/routes/login.routes";
+import { candidatoRoutes } from "../../app/features/candidato/routes/candidato.routes";
+import { vagaRoutes } from "../../app/features/vaga/routes/vaga.routes";
+import { candidaturaRoutes } from "../../app/features/candidatura/routes/candidatura.routes";
 
 export const createApp = () => {
   const app = express();
   app.use(express.json());
   app.use(cors());
+
+  app.use("/recrutador", recrutadorRoutes());
+  app.use("/auth", loginRoutes());
+
+  app.use("/candidato", candidatoRoutes());
+  app.use("/vaga", vagaRoutes());
+
+  app.use("/candidatura", candidaturaRoutes());
 
   return app;
 };
