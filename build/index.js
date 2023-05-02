232ff722da29b8e7d048dc306c5118a7112c0bcc
Gabriela Quadros <ngabiquadros@gmail.com>
2023-05-02 20:50:53 -0300

    moving file

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redis_connection_1 = require("./main/database/redis.connection");
-const typeorm_connection_1 = require("./main/database/typeorm.connection");
-const express_server_1 = require("./main/server/express.server");
-// TypeormConnection.init().then(RedisConnection.connect).then(Server.run);
-Promise.all([typeorm_connection_1.TypeormConnection.init(), redis_connection_1.RedisConnection.connect()]).then(express_server_1.Server.run);
